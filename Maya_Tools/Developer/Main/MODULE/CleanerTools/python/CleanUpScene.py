79f4351c51f06d8f906fe8ecaf145c8450da90d0
trungswat <trungswat@8bed79ed-7f0b-6351-1bc4-4d70257ffec2>
2013-11-10 12:52:34 +0000

    update tools

--- Maya_Tools/Developer/Main/MODULE/CleanerTools/python/CleanUpScene.py
+++ Maya_Tools/Developer/Main/MODULE/CleanerTools/python/CleanUpScene.py
@@ -5,12 +5,12 @@ import maya.cmds as cmds
 import maya.mel as mel
 def execute():
     print '--------------- CLEAN UP SCENE-------------------------'
-    print '\n'
+    
     print '--------------- Optimize scene ------------------------'
-    print '\n'
     mel.eval('OptimizeScene;')
+    
     print '--------------- Convert Instance mesh to object-------------------'
     
-    print '\n'
     print '--------------- Clean up redundant shaders and textures-----------------'
-    print '\n'
+    
+    print '--------------- Clean up dead shape node-----------------'
