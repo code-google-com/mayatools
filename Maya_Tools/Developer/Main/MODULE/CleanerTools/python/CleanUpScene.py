e7663ef033fa4aceb3fc4aaa76d4b75f0b4d087b
trungswat <trungswat@8bed79ed-7f0b-6351-1bc4-4d70257ffec2>
2013-11-11 02:07:07 +0000

    update

--- Maya_Tools/Developer/Main/MODULE/CleanerTools/python/CleanUpScene.py
+++ Maya_Tools/Developer/Main/MODULE/CleanerTools/python/CleanUpScene.py
@@ -3,6 +3,8 @@ name = 'CleanUpScene'
 
 import maya.cmds as cmds
 import maya.mel as mel
+import pymel.core as py
+
 def execute():
     print '--------------- CLEAN UP SCENE-------------------------'
     
@@ -11,6 +13,31 @@ def execute():
     
     print '--------------- Convert Instance mesh to object-------------------'
     
+    
     print '--------------- Clean up redundant shaders and textures-----------------'
     
     print '--------------- Clean up dead shape node-----------------'
+    shapeNodes = py.ls(shapes = True, long = True)
+    liveShapes = list()
+    #print shapeNodes
+    sgs = py.ls(typ = 'shadingEngine')
+    for shape in shapeNodes:
+        for sg in sgs:
+            member = [mem.split('.f')[0] for mem in sg.members()]
+            if shape in member:
+                liveShapes.append(shape)# = [shape for shape in shapeNodes for sg in sgs if py.sets(shape, im = sg)]
+    deadShapes = [shape for shape in shapeNodes if shape not in liveShapes]
+    for node in deadShapes:
+        if node.nodeType() == 'camera':
+            deadShapes.remove(node)
+    for node in deadShapes:
+        if len(node.listConnections(c = True)):
+            deadShapes.remove(node)
+    for dead in deadShapes:
+        try:
+            cmds.delete(dead)
+        except:
+            pass
+
+    
+    
---
+++ Maya_Tools/Developer/Main/MODULE/CleanerTools/python/CorrectNodeName.py
@@ -0,0 +1,33 @@
+description = 'Correct all node name'
+name = 'CorrectShapeNode'
+tooltip = 'Correct node name:\n\t Correct shape node.\n\t Correct shadingGroup node.\n\t Correct texture name.'
+
+import maya.cmds as cmds
+import pymel.core as py
+
+def execute():
+    print '--------------- RENAME SHAPENODE-------------------------'
+    meshes = cmds.ls(type= 'mesh')
+    for mesh in meshes:
+        transformNode = cmds.listRelatives(mesh, parent = True, type = 'transform')[0]
+        if mesh != transformNode + 'Shape':
+            cmds.rename(mesh, transformNode + 'Shape')
+            print '-- Renamed shape node on mesh:' + mesh
+            
+    print '--------------- RENAME SHADINGNODE-------------------------'
+    shaders = py.ls(materials = True)
+    for s in shaders:
+        sg = str(s.listConnections(s= True, t= 'shadingEngine')[0])
+        try:
+            py.rename(sg, s+'SG')
+            print '-- Renamed shading node: ' + sg + ' to: ' + s + 'SG'
+        except:
+            pass
+    
+    print '--------------- RENAME TEXTURENODE-------------------------' 
+    textures = py.ls(tex = True)
+    for t in textures:
+        name = t.getAttr('fileTextureName').split('/')[-1].split('.')[0]
+        py.rename(t, name)   
+        print '-- Renamed texture node: ' + t + ' to: ' + name
+    
