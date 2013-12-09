4bb656b389169c5694efd544286219f7fd6e956a
trungswat <trungswat@8bed79ed-7f0b-6351-1bc4-4d70257ffec2>
2013-12-09 04:41:55 +0000

    update tools

--- Maya_Tools/Developer/Project/Sony/python/checkingContents/setAOForWindows.py
+++ Maya_Tools/Developer/Project/Sony/python/checkingContents/setAOForWindows.py
@@ -6,5 +6,14 @@ import maya.mel as mel
 
 def execute():
     print '--------------- PAINT PURE WHITE FOR AO WINDOWs-------------------------'
-    windowMesh = py.ls('*WINDOW*')
+    windowMesh = [cmds.listRelatives(mesh, c = True)[0] for mesh in cmds.ls('*WINDOW*', type = 'transform')]
+    for mesh in windowMesh:
+        if cmds.polyColorSet(mesh, q= True, acs = True):
+            if 'colorSet1' not in cmds.polyColorSet(mesh, q= True, acs = True):
+                cmds.polyColorSet(mesh, nc = 'colorSet1')
+            else:
+                cmds.polyColorSet(mesh, ccs = True, cs = 'colorSet1')
+        else:
+            cmds.polyColorSet(mesh, nc = 'colorSet1')
+        cmds.polyColorPerVertex(mesh, a = 1)
     
