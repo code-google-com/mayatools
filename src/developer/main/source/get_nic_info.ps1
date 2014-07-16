c5e8fce3aae4337584a0fa3f39546667fd09eb65
trungswat <trungswat@8bed79ed-7f0b-6351-1bc4-4d70257ffec2>
2014-07-16 11:26:56 +0000

    add power shell

---
+++ src/developer/main/source/get_nic_info.ps1
@@ -0,0 +1,18 @@
+﻿function Get-InfoNIC{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)][string]$ComputerName
+    )
+    $nics = Get-WmiObject -Class Win32_NetworkAdapter -ComputerName $ComputerName -Filter "PhysicalAdapter=True"
+    foreach ($nic in $nics){
+        $prop = @{'NICName' = $nic.servicename;
+                  'Speed' = $nic.speed/1MB -as [int];
+                  'Manufacturer' = $nic.manufacturer;
+                  'MACAddress' = $nic.macaddress
+        }
+        New-Object -TypeName PSObject -Property $prop
+    }
+}
+
+
+    
---
+++ src/developer/main/source/get_window_info.ps1
@@ -0,0 +1,12 @@
+﻿function Get-OSVersion{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)][string]$ComputerName
+    )
+    $os = Get-WmiObject -class Win32_OperatingSystem -ComputerName $ComputerName
+    $prop = @{'OSVersion' = $os.version;
+              'SPVersion' = $os.servicepackmajorversion;
+              'OSBuild'   = $os.buildnumber}
+    New-Object -TypeName PSObject -Property $prop
+}
+
